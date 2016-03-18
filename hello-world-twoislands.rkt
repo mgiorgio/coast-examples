a94fba351e15d289e436eac4edf30fb31e24cd34
Matias Giorgio <matias.giorgio@gmail.com>
2016-03-17 19:28:58 -0700

    Hello world example with two islands.

---
+++ hello-world-twoislands.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(require
+  Island/include/base
+)
+
+(define CERTIFICATE/PUBLIC "./certificates/public/")
+(define CERTIFICATE/SECRET "./certificates/secret/")
+(define ALICE/SECRET/PATH (string-append CERTIFICATE/SECRET "alice_secret"))
+(define BOB/SECRET/PATH   (string-append CERTIFICATE/SECRET "bob_secret"))
+
+(define ALICE/CURVE/SECRET (path-to-curve ALICE/SECRET/PATH))
+(define BOB/CURVE/SECRET   (path-to-curve BOB/SECRET/PATH))
+
+(define (islet/hello)
+  (display (format "Hi, I am ~a" (island/nickname (this/island)))))
+
+(define alice (island/new 'alice ALICE/CURVE/SECRET islet/hello))
+(define bob   (island/new 'bob   BOB/CURVE/SECRET   islet/hello))
